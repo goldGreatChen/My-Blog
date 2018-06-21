a659678b2946cc060ddea99c8c1129e179fa1130
chenjinwei <chenjinwei@nidong.com>
2018-06-21 09:51:39 +0800

    fix bug

--- src/main/java/com/my/blog/website/utils/LogAspect.java
+++ src/main/java/com/my/blog/website/utils/LogAspect.java
@@ -1,10 +1,8 @@
 package com.my.blog.website.utils;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -22,23 +20,57 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class LogAspect {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DataSource.class);
+    private static final Logger LOGGE = LoggerFactory.getLogger(LogAspect.class);
 
-    @Pointcut("execution(public * com.my.blog.website.controller..*.*(..))")
-    public void webLog(){}
+    @Pointcut("execution(public * com.my.blog.website.controller.*.*(..))")
+    public void webLog() {
+    }
 
     @Before("webLog()")
-    public void doBefore(JoinPoint joinPoint) throws Throwable {
+    public void deBefore(JoinPoint joinPoint) throws Throwable {
         // 接收到请求，记录请求内容
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
         // 记录下请求内容
-        LOGGER.info("URL : " + request.getRequestURL().toString() + ",IP : " + request.getRemoteAddr() + ",CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName() + ",ARGS : " + Arrays.toString(joinPoint.getArgs()));
+        LOGGE.info("URL : " + request.getRequestURL().toString());
+        LOGGE.info("HTTP_METHOD : " + request.getMethod());
+        LOGGE.info("IP : " + request.getRemoteAddr());
+        LOGGE.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        LOGGE.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+
     }
 
-    @AfterReturning(returning = "object", pointcut = "webLog()")
-    public void doAfterReturning(Object object) throws Throwable {
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
         // 处理完请求，返回内容
-        LOGGER.info("RESPONSE : " + object);
+        LOGGE.info("方法的返回值 : " + ret);
+    }
+
+    //后置异常通知
+    @AfterThrowing("webLog()")
+    public void throwss(JoinPoint jp) {
+        LOGGE.info("方法异常时执行.....");
+    }
+
+    //后置最终通知,final增强，不管是抛出异常或者正常退出都会执行
+    @After("webLog()")
+    public void after(JoinPoint jp) {
+        LOGGE.info("方法最后执行.....");
+    }
+
+    //环绕通知,环绕增强，相当于MethodInterceptor
+    @Around("webLog()")
+    public Object arround(ProceedingJoinPoint pjp) {
+        long start=System.currentTimeMillis();
+
+        try {
+            Object o = pjp.proceed();
+            long end=System.currentTimeMillis();
+            LOGGE.error((end-start)+":请求用时");
+            return o;
+        } catch (Throwable e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
